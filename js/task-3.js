85b824e08b1497e9cc97d34cb5fc004597c69126
romanborynets0 <romanborynets@gmail.com>
2023-12-11 20:15:57 +0200

    first commit

---
+++ js/task-3.js
@@ -0,0 +1,8 @@
+function getElementWidth(content, padding, border) {
+  const borderBox = parseFloat(content) + (parseFloat(padding) + parseFloat(border)) * 2;
+  return borderBox
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
